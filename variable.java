086a00c3f68dc053501ea79310e2cb3cb6e1ee73
AMAN KUMAR <79465043+Aman7080@users.noreply.github.com>
2022-08-19 11:50:20 +0530

    variables in java

---
+++ variable.java
@@ -0,0 +1,15 @@
+/*  Program to demonstrate different type of variable used in Java */
+public class variable {
+    public static void main(String[] args) {
+        int num = 30; // without decimal
+        float decNum = 56.89f; // decimal number
+        char ch = 'a'; // single character
+        String str = "This is String"; // full text
+        boolean bool = true; // either true or false
+        System.out.println("Num = " + num);
+        System.out.println("decNum = " + decNum);
+        System.out.println("ch = " + ch);
+        System.out.println("str = " + str);
+        System.out.println("bool = " + bool);
+    }
+}
