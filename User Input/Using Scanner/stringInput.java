80b2547fb1fed0bcd307ed1fa48042b2431a8eba
AMAN KUMAR <79465043+Aman7080@users.noreply.github.com>
2022-08-19 10:23:22 +0530

    first commit

---
+++ User Input/Using Scanner/stringInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class stringInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string");
+        String str = sc.nextLine();
+        System.out.println(str);
+        sc.close();
+    }
+}
