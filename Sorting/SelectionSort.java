7d360a248c5244b2de91a22c44458d28be94b8bb
Aman Kumar <kumar7080aman7080@gmail.com>
2022-10-11 22:53:43 +0530

    Selection Sort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,28 +1,31 @@
 public class SelectionSort {
-    public static void sort(int arr[]){
-        for (int i = 0; i < arr.length-1; i++) {
-            int minpos = i;
-            System.out.println("Pass "+ i);
-            System.out.println("*************");
-            for (int j = i+1; j < arr.length; j++) {
-                if(arr[j]<arr[minpos]){
-                    minpos =j;
+    public static void sort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int pos = i; //Position of minimum element
+            System.out.println("Pass " + (i + 1));
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[pos]) {
+                    pos = j;
                 }
             }
             int tmp = arr[i];
-            arr[i] = arr[minpos];
-            arr[minpos] = tmp;
+            arr[i] = arr[pos];
+            arr[pos] = tmp;
             display(arr);
-            System.out.println();
         }
-     } 
-     public static void display(int arr[]){
-         for (int i : arr) {
-             System.out.print(i+"\t");
-            }
-     }
-    public static void main(String args[]){
-        int arr[] = {5, 1, 4, 2, 8};
+    }
+
+    public static void display(int arr[]) {
+        for (int i : arr) {
+            System.out.print(i + "\t");
+        }
+        System.out.println();
+        System.out.println("***********************************\n");
+    }
+
+    public static void main(String args[]) {
+      //  int arr[] = { 5, 1, 4, 2, 8 };
+        int arr[]={56,23,67,11,45};
         sort(arr);
     }
 }
